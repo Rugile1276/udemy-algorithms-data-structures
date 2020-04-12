34a10a7ab2dd5c1cc75e518971d90b65661aa532
Your NameRugile <petrukauskaiterugile@gmail.com>
2020-04-12 16:28:36 +0300

    Added bubble sort code

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,3 +1,19 @@
-function bubbleSort (arr){
-
+function swap(arr, val1, val2){
+  var temp = arr[val1];
+  arr[val1] = arr[val2];
+  arr[val2] = temp;
 }
+
+function bubbleSort (arr){
+  for(var i=arr.length; i > 0; i--){
+    for(var j=0; j < i-1;j++){
+      if(arr[j] > arr[j+1]){
+        swap(arr, j, j+1);
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([5, 2, 3, 4, 1]);
+
